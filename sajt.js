263733439f1d8fd523f915fe87772041b5b2c0e2
matematika-profesor <matematikaprofesor.tm@gmail.com>
2024-10-03 20:41:56 +0200

    Update sajt.js

--- sajt.js
+++ sajt.js
@@ -1,29 +1,29 @@
-// Funkcionalnost za prebacivanje između latinice i ćirilice
-document.getElementById('latinButton').addEventListener('click', function() {
-    document.body.classList.remove('cyrillic');
-    document.body.classList.add('latin');
-    document.querySelectorAll('.latin-text').forEach(el => el.classList.remove('hidden'));
-    document.querySelectorAll('.cyrillic-text').forEach(el => el.classList.add('hidden'));
-});
-
-document.getElementById('cyrillicButton').addEventListener('click', function() {
-    document.body.classList.remove('latin');
-    document.body.classList.add('cyrillic');
-    document.querySelectorAll('.cyrillic-text').forEach(el => el.classList.remove('hidden'));
-    document.querySelectorAll('.latin-text').forEach(el => el.classList.add('hidden'));
-});
-
 // Inicijalizacija EmailJS
 (function() {
-    emailjs.init("YOUR_PUBLIC_KEY"); // Zameni sa svojim public key
+    emailjs.init("wcqOEJyjlrYsSLiWn"); // Tvoj public key
 })();
+
+// Prebacivanje između latinice i ćirilice
+document.getElementById('latinButton').onclick = function() {
+    document.body.classList.add('latin');
+    document.body.classList.remove('cyrillic');
+    document.querySelectorAll('.latin-text').forEach(el => el.classList.remove('hidden'));
+    document.querySelectorAll('.cyrillic-text').forEach(el => el.classList.add('hidden'));
+};
+
+document.getElementById('cyrillicButton').onclick = function() {
+    document.body.classList.add('cyrillic');
+    document.body.classList.remove('latin');
+    document.querySelectorAll('.cyrillic-text').forEach(el => el.classList.remove('hidden'));
+    document.querySelectorAll('.latin-text').forEach(el => el.classList.add('hidden'));
+};
 
 // Slanje forme
 const form = document.getElementById('contactForm');
 
 form.addEventListener('submit', function(e) {
     e.preventDefault(); // Sprečava slanje forme
-    emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', this)
+    emailjs.sendForm('service_w7f0n2a', 'template_rt1po5b', this) // Tvoj service ID i template ID
         .then(function(response) {
             console.log('SUCCESS!', response.status, response.text);
             document.getElementById('formResponse').textContent = "Poruka je uspešno poslata!";
